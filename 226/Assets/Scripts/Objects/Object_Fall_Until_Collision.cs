941c236dca5e03a74a226af8c352fae42f23c3fd
chai-tea-404 <chaitang.joshua@gmail.com>
2020-02-10 10:50:45 -0500

    Alpha 1.2.4 Fixes -Fixed GameOverScreen to now fill the entire screen on all resolutions -Meteors in Scene_0 Phase 2 now have Rigidbody2D components and fall until a collider is hit, rather than for a set distance

---
+++ 226/Assets/Scripts/Objects/Object_Fall_Until_Collision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object_Fall_Until_Collision : MonoBehaviour
+{
+    // Causes the object to teleport back to its starting position when it hits another collider
+    // Requires the object to have a rigidbody2d
+    // Fall speed is handled by mass and gravity in the rigidbody2d component
+    Vector2 startingPosition;
+
+    void Start(){
+        startingPosition = transform.position;
+    }
+
+    // Return to start upon collision
+    void OnCollisionEnter2D(Collision2D collision){
+        transform.position = startingPosition;
+    }
+
+}
