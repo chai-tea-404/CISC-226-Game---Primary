31151efec91b2ef21fffbd29a191040811c75135
chai-tea-404 <chaitang.joshua@gmail.com>
2020-03-02 21:30:47 -0500

    Alpha 1.3.4: Player fixes (again) and terrain changes
    Fixed the player collider bugs:
    -Player collider is now a U shaped edge collider, which solves the following bugs:
    -Picking up the same item twice.
    -Getting the top of the player caught on one way platforms.
    -Ghost vertices causing the player to get stuck on random surfaces for no apparent reason.
    
    Some minor additions to terrain:
    -The player can now hold the down arrow to drop through one-way platforms.
    -Terrain now has uses physics materials. This allows for adjustable friction and bounciness by creating new materials for tilemaps/objects.
    -Changes have been made to Scene_0 and Scene_Prefab to reflect these terrain changes.

---
+++ 226/Assets/Scripts/Terrain/One_Way_Platforms.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class One_Way_Platforms : MonoBehaviour
+{
+
+    PlatformEffector2D effector;
+
+    void Start(){
+        effector = GetComponent<PlatformEffector2D>();
+    }
+
+    // When holding down, the player can fall through the one way platforms
+    void Update()
+    {
+        // When down is being held, the surface arc of the platforms becomes 0 (intangible)
+        if (Input.GetKey(KeyCode.DownArrow)){
+            effector.surfaceArc = 0f;
+        }
+        // Once released, it's set back to 90 degrees (collider only active from above)
+        else{
+            effector.surfaceArc = 90f;
+        }
+    }
+}
